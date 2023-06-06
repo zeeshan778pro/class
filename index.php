0262820fc3514db1d30265ed07bf4216491b5a69
zeeshan778pro <111628770+zeeshan778pro@users.noreply.github.com>
2023-06-06 21:50:13 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-my php code is updated
+
+<?php
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+
+?>
